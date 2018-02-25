8fc1eeb5fe1a42e9326d71d364e414bd91496299
Elisabeth Hamel <elisabeth@stereosuper.fr>
2018-02-25 20:12:28 +0100

    :art: js flex + no-js

--- portfolio/gulpfile.js
+++ portfolio/gulpfile.js
@@ -87,7 +87,7 @@ gulp.task('templates', function() {
     return gulp.src('src/templates/**')
     
         .pipe($.prettify({ indent_size: 4 }))
-        //.pipe($.htmlmin({collapseWhitespace: true}))
+        .pipe($.htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('dest'))
         .pipe($.size({title: 'template'}));
 });
