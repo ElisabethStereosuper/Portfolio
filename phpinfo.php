b3f9132ce42f1c22940e959ffa5931ffe5ed65a5
Elisabeth Hamel <elisabeth@stereosuper.fr>
2018-03-03 13:08:09 +0100

    :fire: clean up

--- phpinfo.php
+++
@@ -1,5 +0,0 @@
-H&eacute;bergement <a href="http://www.online.net/">Online</a><br />
-<a href="http://documentation.online.net/fr/hebergement-mutualise/gestion-web/configuration-php?s[]=php5
-">Lien</a> vers la documentation.<br />
-<br /><br />
-<?phpinfo();?>
